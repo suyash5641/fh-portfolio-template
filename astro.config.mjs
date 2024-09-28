97d4da868b5e48ba988465d3fe4469784247e3d7
suyash5641 <suyashjai05@gmail.com>
2024-09-28 12:42:03 +0530

    change site url for netlify testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   // TODO: Set your site URL https://docs.astro.build/en/guides/integrations-guide/sitemap/#usage
   // TODO: Get this from the site-config
-  site: 'https://portfolio.frontendhire.com/',
+  site: "https://fluffy-conkies-755a67.netlify.app/",
   integrations: [tailwind(), sitemap()],
   markdown: {
     shikiConfig: {
